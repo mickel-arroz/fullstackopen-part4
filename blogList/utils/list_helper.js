f9989ac8f76e91131f85b335d3267ded97a298b3
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-05-07 22:32:37 -0400

    Exercise 4.5

--- blogList/utils/list_helper.js
+++ blogList/utils/list_helper.js
@@ -8,7 +8,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  return blogs.reduce((prev, current) => {
+    return prev.likes > current.likes ? prev : current;
+  });
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
