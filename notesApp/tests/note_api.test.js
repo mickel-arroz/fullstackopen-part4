d7b21877fb6d8d324eeaf452748356f836092b38
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-05-08 21:53:30 -0400

    backend async/await

--- notesApp/tests/note_api.test.js
+++ notesApp/tests/note_api.test.js
@@ -1,10 +1,30 @@
-const { test, after, describe } = require("node:test");
+const { test, after, describe, beforeEach } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const Note = require("../models/note");
 
 const api = supertest(app);
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 describe("Notes Api", () => {
   test("notes are returned as json", async () => {
@@ -14,17 +34,17 @@ describe("Notes Api", () => {
       .expect("Content-Type", /application\/json/);
   });
 
-  test("there are two notes", async () => {
+  test.only("there are two notes", async () => {
     const response = await api.get("/api/notes");
 
-    assert.strictEqual(response.body.length, 2);
+    assert.strictEqual(response.body.length, initialNotes.length);
   });
 
-  test("the first note is about HTTP methods", async () => {
+  test.only("the first note is about HTTP methods", async () => {
     const response = await api.get("/api/notes");
 
     const contents = response.body.map((e) => e.content);
-    assert(contents.includes("Test note 1"));
+    assert(contents.includes("HTML is easy"));
   });
 });
 
