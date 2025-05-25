3349762f661cbd68b9943bd7b4251fadb03df783
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-05-25 13:14:27 -0400

    Eliminando el try-catch
    - Downgrade de express 5 al 4, para compatibilidad con express-async-errors.
    - Eliminando try-catch para usar únicamente express-async-errors

--- notesApp/controllers/notes.js
+++ notesApp/controllers/notes.js
@@ -6,29 +6,12 @@ notesRouter.get("/", async (request, response) => {
   response.json(notes);
 });
 
-notesRouter.get("/:id", async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id);
-    if (note) {
-      response.json(note);
-    } else {
-      response.status(404).end();
-    }
-  } catch (exception) {
-    next(exception);
-  }
+notesRouter.delete("/:id", async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
-notesRouter.delete("/:id", async (request, response, next) => {
-  try {
-    await Note.findByIdAndDelete(request.params.id);
-    response.status(204).end();
-  } catch (exception) {
-    next(exception);
-  }
-});
-
-notesRouter.post("/", async (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
   const note = new Note({
@@ -36,11 +19,16 @@ notesRouter.post("/", async (request, response, next) => {
     important: body.important || false,
   });
 
-  try {
-    const savedNote = await note.save();
-    response.status(201).json(savedNote);
-  } catch (exception) {
-    next(exception);
+  const savedNote = await note.save();
+  response.status(201).json(savedNote);
+});
+
+notesRouter.get("/:id", async (request, response) => {
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
 });
 
