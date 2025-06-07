cd6c5a12a493709f731a5ef95d797a10077bf3b7
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-06-07 16:32:34 -0400

    Started Part4D (+install jsonwebtoken)

--- notesApp/app.js
+++ notesApp/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const notesRouter = require("./controllers/notes");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -26,6 +27,8 @@ app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
+
+app.use("/api/login", loginRouter);
 
 app.use("/api/notes", notesRouter);
 
